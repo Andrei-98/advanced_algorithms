5f157d2452cf9c96d2b62bfd26e29c0cbca40a00
Andrei Plotoaga <and1.plotoaga@gmail.com>
2023-01-23 12:36:37 +0100

    have an idea for candy

--- 1_greedy_problems_dp/candy2.cc
+++ 1_greedy_problems_dp/candy2.cc
@@ -25,33 +25,87 @@ int main()
     sort(brats.begin(), brats.end(), greater<ll>());
 
     ll ec{};
+    ll total_ec {};
     vector<ll> ecv{};
     vector<ll> feed_brats{};
 
     ll prev {brats[0]};
     ll next {1};
-    while (ec < candy)
+    // have to keep track of candy here
+    while (total_ec < candy)
     {
         feed_brats.push_back(prev);
-        if(next == brats.size())
-            break;
-        ec += prev - brats[next];
+        ec = prev - brats[next];
+        total_ec += ec;
         ecv.push_back(prev - brats[next]);
+        if(ec > candy / 2)
+        {
+            // ecv.push_back(0);
+            break;
+        }
         prev = brats[next];
         next++;
+        if(next == brats.size())
+            break;
     }
+    // how much candy needs to be paid out for ecv to be same values;
+    total_ec -= ec;
+    candy -= total_ec;
+    cerr << "candy " << candy << endl;
+    cerr << "FIRST ECV = ";
+    for (auto i : ecv)
+    {
+        cerr << i << " ";
+    }
+    cerr << endl;
+    cerr << "candy left = " << candy << endl; 
+
+
+
+
+
+    // if (ec < candy)
+    // {
+    //     candy -= ec + 1;
+    // } else
+    // {
+    //     candy = 0;
+    // }
     
     ecv[ecv.size() - 1] = 0;
 
-    if (ecv.size() % 2 != 0)
-        ecv[0]++;
+
+    // if (candy % ecv.size() != 0)
+    // {
+    //     cerr << "odd number increasing first ecv elem by one candy-- " << candy
+    //          << endl;
+    //     ecv.at(0)++;
+    //     candy--;
+    // }
+    for(ll i {}; i < ecv.size(); i++)
+    {
+        if(candy % ecv.size() == 0)
+            break;
+        else
+        {
+            ecv[i]++;
+            candy--;
+        }
+    }
+
+    // bool modulo{candy % ecv.size() != 0};
+    // ll rest {candy & ecv.size() }
+
+    ll deal_to_ecv {candy / ecv.size()};
+    cerr << "Deal to ecv " << deal_to_ecv << " ";
     
-    cout << "ecv" << endl;
     for (auto & i : ecv)
     {
-        i++;
-        cout << i << endl;
+        i += deal_to_ecv;
+        cerr << i << " " << endl;
+        candy -= deal_to_ecv;
     }
+
 
     // maybe can remove this loop?
     for (ll i {}; i < ecv.size(); i++)
@@ -62,8 +116,13 @@ int main()
     cout << endl;
 
     ll total_ree{};
-    for ( auto const ree : brats)
+    for ( auto ree : brats)
     {
+        if (candy)
+        {
+            ree--;
+            candy--;
+        }
         cout << ree << " ";
         total_ree += (ree * ree);
     }
