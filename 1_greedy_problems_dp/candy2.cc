8bf6064bed97212e17b69ffbb7b7a7353db0de5f
Andrei Plotoaga <and1.plotoaga@gmail.com>
2023-01-25 00:05:25 +0100

    10 4 13 9 7 2 works but 10 5 9 9 9 9 2 or any sequence that is already normalized fails

--- 1_greedy_problems_dp/candy2.cc
+++ 1_greedy_problems_dp/candy2.cc
@@ -7,7 +7,6 @@ typedef int ll;
 
 using namespace std;
 
-
 void print_vector(vector<ll> brats, string const& line="BRATS :")
 {
     cerr << line;
@@ -38,22 +37,83 @@ vector<ll> get_candy_demands()
 }
 
 
-ll smallest_brat(vector<ll> const& brats, ll const candy)
+ll get_smallest_brat(vector<ll> const& brats, ll const candy)
 {
     ll estimated_cost{};
-    ll prev_candy {brats[0]};
+    ll prev_brat {brats[0]};
+    ll index {1};
 
-    for (ll i{1}; i <= brats.size(); i++)
+    for (size_t i{1}; i <= brats.size(); i++)
     {
-        estimated_cost += prev_candy - brats[i];
-        prev_candy = brats[i];
+        estimated_cost += prev_brat - brats[i];
+        prev_brat = brats[i];
+
 
         if(estimated_cost >= candy)
         {
+            index = i - 1; 
             break;
         }
     }
+
+    cerr << "Smallest number is " << brats[index] << endl;
+
+    return index;
 }
+
+
+void normalize_vector(vector<ll> & brats, ll & candy, ll const up_to_index)
+{
+    ll smallest_value {brats[up_to_index]};
+    ll normalization {};
+    cout << "candy " << candy << endl;
+
+    for (int i{0}; i < up_to_index; i++)
+    {
+        normalization = brats[i] - smallest_value;
+        brats[i] -= normalization;
+        cerr << candy << " - " << normalization << endl;
+        candy -= normalization;
+    }
+    cout << "candy _ " << candy << endl;
+}
+
+
+ll get_ree_level(vector<ll> & brats, ll & candy, ll const smallest_brat_index)
+{
+    cout << "up to index " << smallest_brat_index << endl;
+    ll total_candy = candy;
+    bool deal_bags_candy {false};
+
+    if (candy > (int)brats.size())
+    {
+        if(candy % brats.size() != 0)
+            brats[0] -= 1;
+        candy = candy / brats.size();
+        deal_bags_candy = true;
+    }
+
+    ll anger {};
+    for (size_t i{}; i <= brats.size(); i++)
+    {
+        if (total_candy > 0 && deal_bags_candy)
+        {
+            total_candy -= candy;
+            brats[i] -= candy;
+        } else if (total_candy > 0)
+        {
+            brats[i]--;
+            candy--;
+            total_candy--;
+        }
+
+        cerr << "andger " << brats[i] << endl;
+        anger += (brats[i] * brats[i]);
+    }
+
+    return anger;
+}
+
 
 
 int main() 
@@ -62,68 +122,11 @@ int main()
     cin >> candy;
 
     vector<ll> brats {get_candy_demands()};
-
-
-
-
-    // print_vector(ecv, "ecv :");
-    // print_vector(feed_brats, "feed_brats");
-
-
-    
-
-
-    // for(ll i {}; i < ecv.size(); i++)
-    // {
-    //     if(candy % ecv.size() == 0)
-    //         break;
-    //     else
-    //     {
-    //         ecv[i]++;
-    //         candy--;
-    //     }
-    // }
-
-    ll deal_candy {candy / index};
-    for (ll i{0}; i <= index; i++)
-    {
-        brats[i] -= deal_candy;
-        candy -= deal_candy;
-
-    }
+    ll const smallest_brat_index {get_smallest_brat(brats, candy)};
+    cerr << smallest_brat_index << " <" << brats.size() << endl;
+    if(smallest_brat_index != brats.size())
+        normalize_vector(brats, candy, smallest_brat_index);
+    print_vector(brats, "AFTER NORMALIZAION :");
+    cout << get_ree_level(brats, candy, smallest_brat_index) << '\n';
     print_vector(brats);
-
-
-
-    // ll deal_to_ecv {candy / ecv.size()};
-    // cerr << "Deal to ecv " << deal_to_ecv << " ";
-    
-    // for (auto & i : ecv)
-    // {
-    //     i += deal_to_ecv;
-    //     cerr << i << " " << endl;
-    //     candy -= deal_to_ecv;
-    // }
-
-
-    // maybe can remove this loop?
-    // for (ll i {}; i < ecv.size(); i++)
-    // {
-    //     brats[i] -= ecv[i];
-    // }
-
-    ll total_ree{};
-    for ( auto ree : brats)
-    {
-        if (candy)
-        {
-            ree--;
-            candy--;
-        }
-        cout << ree << " ";
-        total_ree += (ree * ree);
-    }
-
-    cout << endl; 
-    cout << total_ree << endl;
 }
